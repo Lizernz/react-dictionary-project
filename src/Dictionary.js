df7404b062e66e799a54f6d88ba194d20f221285
Elizabeth Lavender <lizernz24@gmail.com>
2024-07-31 13:47:57 +0100

    Rendered the meanings and results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
     function Dictionary () {
         let [keyword, setkeyword] = useState("");
+        let [results, setResults] = useState({});
 
         function handleResponse(response) {
             console.log(response.data[0]);
+            setResults(response.data[0]);
     
         }
     
@@ -14,7 +17,7 @@ import "./Dictionary.css";
         function search(event) {
             event.preventDefault();
     
-    
+    // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse)
         }
@@ -29,6 +32,7 @@ import "./Dictionary.css";
         <form onSubmit={search}>
             <input type="search" onChange={handlekeywordChange}/>
         </form>
+        <Results results={results} />
         </div>
     
 }
